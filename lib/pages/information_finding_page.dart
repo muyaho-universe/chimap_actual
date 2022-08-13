7c819d50262509e0a7fa67f9a78b6e0abd443145
muyaho-universe <kimdaesk@gmail.com>
2022-08-13 15:49:28 +0900

    Somewhere

--- lib/pages/information_finding_page.dart
+++ lib/pages/information_finding_page.dart
@@ -17,7 +17,7 @@ class _InfomationCheckState extends State<InfomationCheck> {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-      stream: FirebaseFirestore.instance.collection('mail').snapshots(),
+      stream: FirebaseFirestore.instance.collection('user').snapshots(),
       builder: (BuildContext context,
           AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
@@ -28,13 +28,30 @@ class _InfomationCheckState extends State<InfomationCheck> {
         if (snapshot.hasData) {
           for (int i = 0; i < snapshot.data!.docs.length; i++) {
             var one = snapshot.data!.docs[i];
-            if(!uInfo.id.isEmpty){
-              if(one.get('name')){
+            if(!uInfo.id.isEmpty){      // password finding
+              if(one.get('name') == uInfo.userName
+                  && one.get('phone') == uInfo.userPhoneNum
+              &&one.get('uid') == uInfo.id){
 
               }
             }
-
+            else{     //id finding
+              if(one.get('name') == uInfo.userName && one.get('phone') == uInfo.userPhoneNum){
+                print('야호');
+                uInfo.setID(one.get('id'));
+                Get.toNamed('/first/login/findingIDinfo',arguments: uInfo);
+              }
+              else{
+                // Get.toNamed('/first');
+              }
+            }
+            return Center(
+              child: CircularProgressIndicator(), //로딩되는 동그라미 보여주기
+            );
           }
+          return Center(
+            child: CircularProgressIndicator(), //로딩되는 동그라미 보여주기
+          );
         }
 
         return Center(
